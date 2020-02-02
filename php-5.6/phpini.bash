935b94744dbdec19581114faf45ac82ad8e9d3ee
Kevin Williams <llslim@gmail.com>
2020-02-01 20:21:53 -0500

    initial commit

---
+++ php-5.6/phpini.bash
@@ -0,0 +1,1 @@
+curl -sS https://raw.githubusercontent.com/php/php-src/PHP-5.6.30/php.ini-development - > php.ini-development
---
+++ php-7.2/phpini.bash
@@ -0,0 +1,1 @@
+Curl -Ss https://raw.githubusercontent.com/php/php-src/PHP-7.0/php.ini-development - > php.ini-development
