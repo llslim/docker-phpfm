c51e86fecaae92dfae269950e24f5e1bca89a781
Kevin Williams <llslim@gmail.com>
2020-03-17 20:22:05 -0400

    purge everything but 7.3-phpfpm

--- 5.6-apache/phpini.bash
+++
@@ -1,1 +0,0 @@
-curl -sS https://raw.githubusercontent.com/php/php-src/PHP-5.6.30/php.ini-development - > php.ini-development
--- 7.3-apache/phpini.bash
+++
@@ -1,1 +0,0 @@
-Curl -Ss https://raw.githubusercontent.com/php/php-src/PHP-7.0/php.ini-development - > php.ini-development
